5fbf38466776417fd25100e609035fbad9fe69f8
Max Sharov <maxsharoff@gmail.com>
2026-04-14 10:22:25 +0200

    ci(release): add beta prerelease branch

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,9 @@
 import process from "node:process";
 
 export default {
-  branches: [process.env.DEFAULT_BRANCH || "master"],
+  branches: [
+    process.env.DEFAULT_BRANCH || "master",
+    { name: "beta", prerelease: true },
+  ],
   tagFormat: "${version}",
 };
